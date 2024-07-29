6787d2dd0a947059d8195a375962f7a1d1e1e495
Anupam Thackar <anupamthackar67@gmail.com>
2024-07-29 10:42:57 +0530

    Commit5

---
+++ NeoStore Training App/Screens/Home/View/Image Slider/ImageSliderCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSliderCollectionViewCell.swift
+//  NeoStore Training App
+//
+//  Created by Neosoft on 26/07/24.
+//
+
+import UIKit
+
+class ImageSliderCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
